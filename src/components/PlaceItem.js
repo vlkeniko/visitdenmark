b29a32707d88228ecb8f0394ace9d64cc61d4c9d
Eniko Valko <eaaenva@students.eaaa.dk>
2022-11-08 14:04:35 +0100

    1 push
    pwa template, fonts and logos imported too

---
+++ src/components/PlaceItem.js
@@ -0,0 +1,13 @@
+/*
+*PlaceItem
+*Simple Component Function to stype and return a place to eat
+*
+*/
+
+
+export default function PlaceItem({place}){
+    return <div style={{display:"float"}}>
+            <div style={{float:"left", width:"25%"}}>{place.Name}</div>
+            <div style={{float:"left", width:"75%"}}>{place.Category.Name}</div>
+        </div>;
+}
---
+++ src/components/Places.js
@@ -0,0 +1,51 @@
+/*Places
+*
+*Component Functions to fetch and parse json data, example showing and writing filtering of categors
+*Importing and Calling each place in a list of places to be rendered
+*
+*Author Jeffrey Serio
+*/
+
+import React, { useEffect, useState } from 'react'
+import PlaceItem from "./PlaceItem";
+
+export default function Places()
+{
+    const[places, setPlaces] = useState([]);
+
+    useEffect(() => {
+
+        async function getPlaces()
+        {
+            const url = "https://raw.githubusercontent.com/manypossibles/designops/master/assets/data/en/data.json";
+            const response = await fetch(url);
+            const data = await response.json();
+            const filteredrest = [];
+          
+           try{
+                for (var i = 0; i < data.length; i++) {
+                var place = data[i];
+               if(place.MainCategory.Id == '62')
+               {
+                   // console.log(place.Name + ' ' + place.MainCategory.Name);
+                    filteredrest.push(place)
+               }
+            }
+           }catch (error) {
+            console.log('Oh come on! These should be easy peeps! What')
+          }
+            setPlaces(filteredrest);
+        }
+        getPlaces();
+
+    }, []);
+
+    return(
+    <>
+    <h2>Places To Eat</h2>
+        {
+           places.map(place =>(<PlaceItem place={place} key={place.id} />))
+        }
+    </>
+    )    
+}
