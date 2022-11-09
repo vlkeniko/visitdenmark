b3ec6309e7597dccb536bcf3a440187d2d1fb6ff
Eniko Valko <eaaenva@students.eaaa.dk>
2022-11-09 09:59:27 +0100

    connections on  other page i guess

--- src/pages/OtherPage.jsx
+++ src/pages/OtherPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../components/nav'
+import Places from '../components/Places'
 
 export default function OtherPage() {
   return (
     <>
     <div>OtherPage</div>
     <Nav/>
+    <Places/>
     </>
     
   )
