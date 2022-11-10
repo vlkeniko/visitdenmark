f7b0d1c895ed80c147f8f276e64e3187c372847c
Eniko Valko <eaaenva@students.eaaa.dk>
2022-11-10 09:52:37 +0100

    +description component

---
+++ src/components/DiscountDescription.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DiscountDescription() {
+  return (
+    <div>Use your points to get discounts in many stores!</div>
+  )
+}
--- src/pages/DiscountPage.jsx
+++ src/pages/DiscountPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import DiscountDescription from '../components/DiscountDescription'
 import DiscountList from '../components/DiscountList'
 import Nav from '../components/nav'
 export default function DiscountPage() {
   return (
     <>
         <div>DiscountPage</div>
+        <DiscountDescription/>
         <DiscountList/>
     <Nav/>
     </>
