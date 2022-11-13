d219c393356fac65456f6dc9a3251e2cfb72381a
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-11-13 19:01:46 +0100

    Location data test, formatting

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import Places from '../components/Places';
-import Attractions from '../components/Attractions';
-import Activities from '../components/Activities'
-import Events from '../components/Events'
+import React from "react";
+import Places from "../components/Places";
+import Attractions from "../components/Attractions";
+import Activities from "../components/Activities";
+import Events from "../components/Events";
 import Header from "../components/Header";
-import Nav from '../components/nav'
+import Nav from "../components/nav";
 // import Places from '../components/Places'
+import { getLocation } from "../utils/GeoLocation";
 
 export default function ExplorePage() {
+  getLocation();
+
   return (
     <>
-    <Header />
-      <Attractions/>
-      <Places/>
-      <Activities/>
-      <Events/>
+      <Header />
+      <Attractions />
+      <Places />
+      <Activities />
+      <Events />
       <Nav />
     </>
   );
