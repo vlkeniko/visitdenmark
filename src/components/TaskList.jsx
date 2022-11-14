05961fb74adf2925c5a339649b946223ba8838d9
Eniko Valko <eaaenva@students.eaaa.dk>
2022-11-14 22:56:21 +0100

    Content for the tasks
    very basic

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function TaskList() {
+  return (
+  
+    <div>
+          {/*From wonderful w3schools...*/}
+        <label class="container">Find a stranger and drink a shot with them
+  <input type="checkbox"/>
+  <span class="checkmark"></span>
+</label>
+
+<label class="container">Find someone who works for the city,
+get their autograph & take a selfie
+  <input type="checkbox"/>
+  <span class="checkmark"></span>
+</label>
+
+<label class="container">Go to a restaurant and make them give you
+a local dish for free
+  <input type="checkbox"/>
+  <span class="checkmark"></span>
+</label>
+
+<label class="container">Quiz
+  <input type="checkbox"/>
+  <span class="checkmark"></span>
+</label>
+    </div>
+  )
+}
--- src/pages/playtasks/FamilyTasks.jsx
+++ src/pages/playtasks/FamilyTasks.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import playimage from "../../play-image.png";
+import TaskList from "../../components/TaskList";
 
 export default function FamilyTasks() {
   return (
-    <div>FamilyTasks</div>
-  )
+    <div>
+      <div className="img-gradient">
+        <img className="play-image" src={playimage} alt="fam" />
+      </div>
+      <h1>Family</h1>
+      <p>Tasks</p>
+      <p className="completed-tasks">3 / 8 tasks completed | 260 pts </p>
+      <TaskList />
+    </div>
+  );
 }
