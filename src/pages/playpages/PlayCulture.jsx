6a572d3b3f8a59e7d106b99ad812a6697b934eba
Eniko Valko <eaaenva@students.eaaa.dk>
2022-11-14 22:07:09 +0100

    Some content on the play pages

--- src/pages/playpages/PlayCulture.jsx
+++ src/pages/playpages/PlayCulture.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import Nav from '../../components/nav'
+import Nav from "../../components/nav";
+import playimage from "../../play-image.png";
 
 export default function PlayCulture() {
   return (
-    <div>PlayCulture
-        <Nav/>
+    <div>
+      <img className="play-image" src={playimage} alt="fam" />
+      <h1>Culture</h1>
+      <p className="play-description">
+        Whether you’re looking for city planners, activities to enjoy, or you
+        just search for a fun way to sightsee in a new city, scavenger hunts are
+        a lot of fun. Plus, you can use GPS coordinates, use clever clues, and
+        other fun concepts to get to know random people, to think and solve
+        riddles to get many discounts that you can use in places around the
+        city.
+      </p>
+      <button className="start-button">START</button>
+      <Nav />
     </div>
   )
 }
