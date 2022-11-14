68128fce22b8aa92fe6fc8471584a9c71b61618d
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-11-14 10:09:42 +0100

    Added distance comparison function

--- src/utils/GeoLocation.js
+++ src/utils/GeoLocation.js
@@ -9,7 +9,7 @@ export function getLocation() {
     result.success = false;
   }
 
-  console.log(result);
+  //console.log(result);
   return result;
 }
 
@@ -18,6 +18,77 @@ export function showPosition(position) {
   result.latitude = position.coords.latitude;
   result.longitude = position.coords.longitude;
 
-  console.log(result);
+  //console.log(result);
+  console.log(
+    getDistanceFromLatLonInKm(
+      result.latitude,
+      result.longitude,
+      59.3225525,
+      13.4619422
+    ).toFixed(1)
+  );
   return result;
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+watchPosition({ latitude: 59.3225525, longitude: 13.4619422 });
+
+function watchPosition(target) {
+  console.log("🚀 ~ file: GeoLocation.js ~ line 55 ~ watchPosition ~ watchPosition", watchPosition)
+  console.log("🚀 ~ file: GeoLocation.js ~ line 55 ~ watchPosition ~ target", target)
+  
+  let id;
+  let options;
+
+  id = navigator.geolocation.watchPosition(success, error, options);
+
+  options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 30000,
+  };
+
+  function success(pos) {
+    const crd = pos.coords;
+
+    if (
+      target.latitude === crd.latitude &&
+      target.longitude === crd.longitude
+    ) {
+      console.log("Congratulations, you reached the target");
+      navigator.geolocation.clearWatch(id);
+    }
+
+    console.log(crd);
+    console.log(
+      getDistanceFromLatLonInKm(
+        crd.latitude,
+        crd.longitude,
+        target.latitude,
+        target.longitude
+      ).toFixed(1)
+    );
+  }
+
+  function error(err) {
+    console.error(`ERROR(${err.code}): ${err.message}`);
+  }
 }
